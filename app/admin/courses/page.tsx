03e1f5ac0e5971f5c573863e8d1ea909f365d9fb
Ahmed Nasser <nasserahmed093@gmail.com>
2025-06-20 19:06:31 +0300

    updatin prisma schema

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,5 +1,18 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
-  return <div>hello courses</div>;
+  return (
+    <>
+    <div className="flex items-center justify-between">
+      <h1 className="text-2xl font-bold">Your Courses</h1>
+      <Link href="/admin/courses/create" className={buttonVariants()}>Create Course</Link>
+    </div>
+    <div>
+      Here you will see all of the courses
+    </div>
+    </>
+  );
 };
 
 export default CoursesPage;
