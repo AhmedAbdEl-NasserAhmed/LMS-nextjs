f7b07bd2aa7bdaa8724a838b2f90be236ab08ba6
Ahmed Nasser <nasserahmed093@gmail.com>
2025-07-08 02:06:09 +0300

    finishing dnd

--- app/admin/courses/[courseId]/edit/actions.ts
+++ app/admin/courses/[courseId]/edit/actions.ts
@@ -6,6 +6,7 @@ import { ApiResponse } from "@/lib/type";
 import { courseSchema, courseSchemaType } from "@/lib/zodSchema";
 import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
 import { request } from "@arcjet/next";
+import { revalidatePath } from "next/cache";
 
 const aj = arcjet
   .withRule(
@@ -69,6 +70,89 @@ export async function editCourse(
     return {
       status: "Error",
       message: "Failed to update course"
+    };
+  }
+}
+export async function reorderLesson(
+  chapterId: string,
+  lessons: { id: string; position: number }[],
+  courseId: string
+): Promise<ApiResponse> {
+  await requireAdmin();
+  try {
+    if (!lessons || lessons.length === 0) {
+      return {
+        status: "Error",
+        message: "No lessons provided for reordering"
+      };
+    }
+
+    const updates = lessons.map((lesson) =>
+      prisma.lessson.update({
+        where: {
+          id: lesson.id,
+          chapterId
+        },
+        data: {
+          position: lesson.position
+        }
+      })
+    );
+
+    await prisma.$transaction(updates);
+
+    revalidatePath(`/admin/courses/${courseId}/edit`);
+
+    return {
+      status: "Success",
+      message: "Lesson reordered successfully"
+    };
+  } catch {
+    return {
+      status: "Error",
+      message: "Failed to reorder Lessons "
+    };
+  }
+}
+
+export async function reorderChapter(
+  courseId: string,
+  chapters: { id: string; position: number }[]
+): Promise<ApiResponse> {
+  await requireAdmin();
+
+  try {
+    if (!chapters || chapters.length === 0) {
+      return {
+        status: "Error",
+        message: "No chapters provided for reordering"
+      };
+    }
+
+    const updates = chapters.map((chapter) =>
+      prisma.chapter.update({
+        where: {
+          id: chapter.id,
+          courseId
+        },
+        data: {
+          position: chapter.position
+        }
+      })
+    );
+
+    await prisma.$transaction(updates);
+
+    revalidatePath(`/admin/courses/${courseId}/edit`);
+
+    return {
+      status: "Success",
+      message: "Chpaters reordered successfully"
+    };
+  } catch {
+    return {
+      status: "Error",
+      message: "Failed to redorder chapters"
     };
   }
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main>{children}</main>
-          <Toaster />
+          <Toaster closeButton />
         </ThemeProvider>
       </body>
     </html>
